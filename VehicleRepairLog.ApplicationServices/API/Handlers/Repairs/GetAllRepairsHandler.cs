a5ed6f67d10f01113dd7568ee3f1100eec67cada
Oskar Jasinski <skajlet77@gmail.com>
2022-02-07 18:51:32 +0100

    added repairs controller and handled get all Request

---
+++ VehicleRepairLog.ApplicationServices/API/Handlers/Repairs/GetAllRepairsHandler.cs
@@ -0,0 +1,42 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using VehicleRepairLog.ApplicationServices.API.Domain.Requests.Repairs;
+using VehicleRepairLog.ApplicationServices.API.Domain.Responses.Repairs;
+using VehicleRepairLog.DataAccess;
+using VehicleRepairLog.DataAccess.Entites;
+
+namespace VehicleRepairLog.ApplicationServices.API.Handlers.Repairs
+{
+    public class GetAllRepairsHandler : IRequestHandler<GetAllRepairsRequest, GetAllRepairsResponse>
+    {
+        private readonly IRepository<Repair> repairRepository;
+
+        public GetAllRepairsHandler(IRepository<Repair> repairRepository)
+        {
+            this.repairRepository = repairRepository;
+        }
+
+        public Task<GetAllRepairsResponse> Handle(GetAllRepairsRequest request, CancellationToken cancellationToken)
+        {
+            var repairs = this.repairRepository.GetAll();
+            var repairsModel = repairs.Select(x => new Domain.Models.Repair
+            {
+                Date = x.Date,
+                Description = x.Description,
+                CarWorkshopName = x.CarWorkshopName
+            });
+
+            var response = new GetAllRepairsResponse()
+            {
+                Data = repairsModel.ToList()
+            };
+
+            return Task.FromResult(response);
+        }
+    }
+}
---
+++ VehicleRepairLog/Controllers/RepairsController.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using VehicleRepairLog.ApplicationServices.API.Domain.Requests.Repairs;
+
+namespace VehicleRepairLog.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class RepairsController : ControllerBase
+    {
+        private readonly IMediator mediator;
+
+        public RepairsController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public async Task<IActionResult> GetAllRepairs([FromQuery] GetAllRepairsRequest request)
+        {
+            var response = await this.mediator.Send(request);
+            return Ok(response);
+        }
+    }
+}
