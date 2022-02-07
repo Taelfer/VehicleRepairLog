4d32956c4280c0aec95dc4915ea24479702c863e
Oskar Jasinski <skajlet77@gmail.com>
2022-02-07 18:53:15 +0100

    added user controller

---
+++ VehicleRepairLog/Controllers/UsersController.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VehicleRepairLog.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IMediator mediator;
+
+        public UsersController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+
+    }
+}
