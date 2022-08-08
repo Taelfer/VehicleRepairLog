97061139567b66ae0defbc90bf2ccfe141a41b27
Oskar Jasinski <skajlet77@gmail.com>
2022-08-08 21:10:48 +0200

    Add additional properties to User model on UI side

--- src/VehicleRepairLogUI/Models/User.cs
+++ src/VehicleRepairLogUI/Models/User.cs
@@ -1,8 +1,15 @@
-﻿namespace VehicleRepairLogUI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VehicleRepairLogUI.Models
 {
     public class User
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Username { get; set; }
+        public string Email { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string role { get; set; }
         public bool IsAuthenticated { get; set; }
     }
 }
