5162e870aa41777c2d5fa0646304f09f29514f3d
Oskar Jasinski <skajlet77@gmail.com>
2022-08-23 22:14:05 +0200

    Added Endpoint for Deleting User Accounts to User Controller

--- src/VehicleRepairLog.API/Controllers/UsersController.cs
+++ src/VehicleRepairLog.API/Controllers/UsersController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using VehicleRepairLog.Application.Features.Users;
+using VehicleRepairLog.Application.Features.Users.DeleteUser;
 using VehicleRepairLog.Application.Models;
 
 namespace VehicleRepairLog.API.Controllers
@@ -18,39 +19,79 @@ namespace VehicleRepairLog.API.Controllers
             _mediator = mediator;
         }
 
+        /// <summary>
+        ///     Endpoint for creating User Accounts.
+        /// </summary>
         [AllowAnonymous]
         [HttpPost("register")]
         public async Task<IActionResult> RegisterUser([FromBody] RegisterUserCommand command)
         {
+            // Sends Command with data from HTTP Request to Handler. Receives response.
             RegisterResultDto response = await _mediator.Send(command);
+
+            // Returns HTTP Response with status code 200(OK).
             return Ok(response);
         }
 
+        /// <summary>
+        ///     Endpoint for User Authentication.
+        /// </summary>
         [AllowAnonymous]
         [HttpPost("authenticate")]
         public async Task<IActionResult> AuthenticateUser([FromBody] LoginRequestDto loginRequest)
         {
+            // Assigns data from HTTP Request to Command.
             AuthenticateUserCommand command = new()
             {
                 Password = loginRequest.Password,
                 Email = loginRequest.Email
             };
 
+            // Sends Command with given data to Handler. Receives response.
             LoginResultDto response = await _mediator.Send(command);
+
+            // Returns HTTP Response with status code 200(OK).
             return Ok(response);
         }
 
+        /// <summary>
+        ///     Endpoint for getting User data of given ID.
+        /// </summary>
         [Authorize]
         [HttpGet("{userId}")]
         public async Task<IActionResult> GetUserById([FromRoute] int userId)
         {
+            // Assigns data from HTTP Request to Command.
             GetUserByIdQuery query = new()
             {
                 UserId = userId
             };
 
+            // Sends Query with given data to Handler. Receives response.
             UserDto response = await _mediator.Send(query);
+
+            // Returns HTTP Response with status code 200(OK).
             return Ok(response);
+        }
+
+        /// <summary>
+        ///     Endpoint for deleting User Accounts.
+        /// </summary>
+        [Authorize]
+        [HttpDelete("{userId}")]
+        public async Task<IActionResult> DeleteUser([FromRoute] int userId)
+        {
+            // Assigns data from HTTP Request to Command.
+            DeleteUserCommand command = new()
+            {
+                UserId = userId
+            };
+
+            // Sends Command with given data to Handler.
+            await _mediator.Send(command);
+
+            // Returns HTTP Response with status code 204(No Content).
+            return NoContent();
         }
     }
 }
--- src/VehicleRepairLog.Application/Features/Users/DeleteUser/DeleteUserCommandHandler.cs
+++ src/VehicleRepairLog.Application/Features/Users/DeleteUser/DeleteUserCommandHandler.cs
@@ -10,11 +10,17 @@ using VehicleRepairLog.Infrastructure.Entities;
 
 namespace VehicleRepairLog.Application.Features.Users.DeleteUser
 {
+    /// <summary>
+    ///     Passes data between HTTP Requests and MediatR handler.
+    /// </summary>
     public class DeleteUserCommand : IRequest<UserDto>
     {
-        public int userId;
+        public int UserId;
     }
 
+    /// <summary>
+    ///     Deletes <see cref="User"/> from Database.
+    /// </summary>
     internal class DeleteUserCommandHandler : IRequestHandler<DeleteUserCommand, UserDto>
     {
         private readonly IMapper _mapper;
@@ -28,7 +34,7 @@ namespace VehicleRepairLog.Application.Features.Users.DeleteUser
 
         public async Task<UserDto> Handle(DeleteUserCommand request, CancellationToken cancellationToken)
         {
-            // Returns User data with given ID from DB.
+            // Returns User data with given ID from Database.
             User user = await _context.Users.FirstOrDefaultAsync(user => user.Id == request.userId);
 
             if (user is null)
@@ -36,10 +42,10 @@ namespace VehicleRepairLog.Application.Features.Users.DeleteUser
                 throw new NotFoundException("User has not been found.");
             }
 
-            // Removes User of given ID from DB.
+            // Removes User of given ID from Database.
             _context.Users.Remove(user);
 
-            // Saves changes made to DB.
+            // Saves changes made to Database.
             await _context.SaveChangesAsync();
             
             return _mapper.Map<UserDto>(user);
