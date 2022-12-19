aff38263dacd4566b4bcfda9090758bf2a4910dd
Oskar Jasinski <skajlet77@gmail.com>
2022-12-19 19:18:11 +0100

    Code cleanup

--- src/VehicleRepairLog.Application/Features/Users/RegisterUser/RegisterUserCommandValidator.cs
+++ src/VehicleRepairLog.Application/Features/Users/RegisterUser/RegisterUserCommandValidator.cs
@@ -8,18 +8,25 @@ namespace VehicleRepairLog.Application.Features.Users
     {
         public RegisterUserCommandValidator(VehicleProfileStorageContext dbContext)
         {
-            RuleFor(user => user.Email).EmailAddress().NotEmpty();
+            RuleFor(user => user.Email)
+                .EmailAddress()
+                .NotEmpty();
+                
+            RuleFor(user => user.Password)
+                .NotEmpty()
+                .MinimumLength(8)
+                .MaximumLength(100);
 
-            RuleFor(user => user.Password).NotEmpty().MinimumLength(8).MaximumLength(100);
+            RuleFor(user => user.ConfirmPassword).Equal(command => command.Password);
 
-            RuleFor(user => user.ConfirmPassword).Equal(e => e.Password);
-
-            RuleFor(user => user.Username).MaximumLength(100).NotEmpty();
+            RuleFor(user => user.Username)
+                .MaximumLength(100)
+                .NotEmpty();
 
             RuleFor(user => user.Email)
                 .Custom((value, context) =>
                 {
-                    bool emailInUse = dbContext.Users.Any(x => x.Email == value);
+                    bool emailInUse = dbContext.Users.Any(user => user.Email == value);
 
                     if (emailInUse)
                     {
@@ -30,7 +37,7 @@ namespace VehicleRepairLog.Application.Features.Users
             RuleFor(user => user.Username)
                 .Custom((value, context) =>
                 {
-                    bool usernameInUse = dbContext.Users.Any(x => x.Username == value);
+                    bool usernameInUse = dbContext.Users.Any(user => user.Username == value);
 
                     if (usernameInUse)
                     {
