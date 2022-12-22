b7348e89102fa979e5a24761efdb06757e6e7c2e
Oskar Jasinski <skajlet77@gmail.com>
2022-12-23 00:40:21 +0100

    Created handler for user update

---
+++ src/VehicleRepairLog.Application/Features/Users/UpdateUser/UpdateUserCommandHandler.cs
@@ -0,0 +1,56 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using VehicleRepairLog.Application.Exceptions;
+using VehicleRepairLog.Application.Models;
+using VehicleRepairLog.Infrastructure;
+using VehicleRepairLog.Infrastructure.Entities;
+
+namespace VehicleRepairLog.Application.Features.Users.UpdateUser
+{
+    public class UpdateUserCommand : IRequest<UserDto>
+    {
+        public int UserId;
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public string Role { get; set; }
+        public List<string> Vehicles { get; set; }
+    }
+
+    internal class UpdateUserCommandHandler : IRequestHandler<UpdateUserCommand, UserDto>
+    {
+        private readonly IMapper _mapper;
+        private readonly VehicleProfileStorageContext _context;
+
+        public UpdateUserCommandHandler(IMapper mapper, VehicleProfileStorageContext context)
+        {
+            _mapper = mapper;
+            _context = context;
+        }
+
+        public async Task<UserDto> Handle(UpdateUserCommand request, CancellationToken cancellationToken)
+        {
+            User user = await _context.Users.FirstOrDefaultAsync(user => user.Id == request.UserId);
+
+            if (user is null)
+            {
+                throw new NotFoundException("User not found.");
+            }
+
+            User updatedUser = _mapper.Map(request, user);
+            _context.Users.Update(updatedUser);
+            await _context.SaveChangesAsync();
+
+            return _mapper.Map<UserDto>(user);
+        }
+    }
+}
