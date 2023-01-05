76800aee8c976dbacf7051c2de8f4b52a140288d
Oskar Jasinski <skajlet77@gmail.com>
2023-01-05 08:58:02 +0100

    Removed unecessary fields validation

--- src/VehicleRepairLog.Application/Features/Users/UpdateUser/UpdateUserCommandValidator.cs
+++ src/VehicleRepairLog.Application/Features/Users/UpdateUser/UpdateUserCommandValidator.cs
@@ -1,5 +1,4 @@
 ﻿using FluentValidation;
-using System.Linq;
 using VehicleRepairLog.Infrastructure;
 
 namespace VehicleRepairLog.Application.Features.Users.UpdateUser
@@ -12,38 +11,9 @@ namespace VehicleRepairLog.Application.Features.Users.UpdateUser
                 .EmailAddress()
                 .NotEmpty();
 
-            RuleFor(user => user.Password)
-                .NotEmpty()
-                .MinimumLength(8)
-                .MaximumLength(100);
-
-            RuleFor(user => user.ConfirmPassword).Equal(command => command.Password);
-
             RuleFor(user => user.Username)
                 .MaximumLength(100)
                 .NotEmpty();
-
-            RuleFor(user => user.Email)
-                .Custom((value, context) =>
-                {
-                    bool emailInUse = dbContext.Users.Any(user => user.Email == value);
-
-                    if (emailInUse)
-                    {
-                        context.AddFailure("This Email is already in use.");
-                    }
-                });
-
-            RuleFor(user => user.Username)
-                .Custom((value, context) =>
-                {
-                    bool usernameInUse = dbContext.Users.Any(user => user.Username == value);
-
-                    if (usernameInUse)
-                    {
-                        context.AddFailure("This username is taken.");
-                    }
-                });
         }
     }
 }
