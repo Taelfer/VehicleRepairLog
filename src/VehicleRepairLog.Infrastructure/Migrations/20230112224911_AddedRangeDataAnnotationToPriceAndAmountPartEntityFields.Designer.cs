5c780f325914994ca38330419b53d704cb8aedef
Oskar Jasinski <skajlet77@gmail.com>
2023-01-12 23:50:15 +0100

    Added range data annotation to Price and Amount fields of Part entity

--- src/VehicleRepairLog.Infrastructure/Migrations/20230112224911_AddedRangeDataAnnotationToPriceAndAmountPartEntityFields.Designer.cs
+++ src/VehicleRepairLog.Infrastructure/Migrations/20230112224911_AddedRangeDataAnnotationToPriceAndAmountPartEntityFields.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using VehicleRepairLog.Infrastructure;
 
@@ -11,9 +12,10 @@ using VehicleRepairLog.Infrastructure;
 namespace VehicleRepairLog.Infrastructure.Migrations
 {
     [DbContext(typeof(VehicleRepairLogContext))]
-    partial class VehicleRepairLogContextModelSnapshot : ModelSnapshot
+    [Migration("20230112224911_AddedRangeDataAnnotationToPriceAndAmountPartEntityFields")]
+    partial class AddedRangeDataAnnotationToPriceAndAmountPartEntityFields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
